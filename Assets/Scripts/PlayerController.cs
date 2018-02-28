a7ea6db9831c2beaec237a35f9669558950c12d1
nuhat <nt6888@bristol.ac.uk>
2018-02-28 22:12:10 +0000

    changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,17 @@ public class PlayerController : MonoBehaviour
     public int maxX;
     public int maxY;
     public GameObject scoreTextGO;
+    public GameObject healthTextGO;
+    public GameObject gameOverTextGO;
+    public int health;
+    public int asteroidHealthDeduction;
 
     //Current score
     private int score;
     private Rigidbody2D rb;
     private Text scoreText;
+    private Text healthText;
+    private Text gameOverText;
     private int nextScoreUpdate;
 
     // Use this for initialization
@@ -24,12 +30,21 @@ public class PlayerController : MonoBehaviour
         this.nextScoreUpdate = 1;
         this.rb = gameObject.GetComponent<Rigidbody2D>();
         this.scoreText = scoreTextGO.GetComponent<Text>();
+        this.healthText = healthTextGO.GetComponent<Text>();
+        gameOverText = gameOverTextGO.GetComponent<Text>();
+        gameOverText.text = "";
+        gameOverText.enabled = false;
         SetScoreText();
+        SetHealthText();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetHealthText();
+        if (this.health <= 0)
+            gameOver();
+
         float x = 0;
         if (Input.GetKey("up") && rb.transform.position.y < camera.transform.position.y + maxY)
             transform.position = new Vector2(this.transform.position.x, this.transform.position.y + movementStrength / 100);
@@ -45,15 +60,7 @@ public class PlayerController : MonoBehaviour
             nextScoreUpdate = Mathf.FloorToInt(Time.time) + 1;
             handleScore();
         }
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Coin")
-        {
-            this.score++;
-            collision.gameObject.SetActive(false);
-        }
     }
     
     private void SetScoreText()
@@ -61,9 +68,29 @@ public class PlayerController : MonoBehaviour
         scoreText.text = "Score: " + this.score.ToString();
     }
 
+    private void SetHealthText()
+    {
+        healthText.text = "Health: " + this.health.ToString();
+    }
+
     private void handleScore()
     {
         this.score++;
         SetScoreText();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Hazard")
+        {
+            this.health = this.health - this.asteroidHealthDeduction;
+        }
+    }
+
+    private void gameOver()
+    {
+        this.gameOverText.text = "GAME OVER";
+        this.gameOverText.enabled = true;
+        this.gameObject.SetActive(false);
     }
 }
