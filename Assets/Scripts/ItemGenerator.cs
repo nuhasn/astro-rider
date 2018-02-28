531bf3c4c1ccd48a683580b1e7aeddde0288dcae
nuhat <nt6888@bristol.ac.uk>
2018-02-28 20:59:06 +0000

    sorry :^(

---
+++ Assets/Scripts/ItemGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+    // Item game object to generate.
+    public GameObject item;
+
+    // Max item generation Y position.
+    public int maxY;
+
+    // Monitor next point that the item needs to generated at.
+    private int nextGenerationPoint;
+
+	// Use this for initialization
+	void Start () {
+        this.nextGenerationPoint = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= nextGenerationPoint)
+        {
+            nextGenerationPoint = Mathf.FloorToInt(Time.time) + 1;
+            GameObject ob = (GameObject) Instantiate (item);
+            ob.SetActive(true);
+            Vector2 position = this.transform.position;
+            ob.transform.position = new Vector2(position.x, Random.Range(position.y - maxY, position.y + maxY));
+        }
+    }
+}
---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    // Destroy the object after it reaches this point.
+    private GameObject destructPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        destructPoint = GameObject.FindWithTag("Finish");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x < destructPoint.transform.position.x)
+        {
+            Debug.Log("here");
+            Destroy(gameObject);
+        }
+    }
+}
